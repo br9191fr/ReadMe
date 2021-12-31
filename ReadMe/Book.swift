0062dae55ad134b73067a9ebae51c8b29ebef1bd
Bruno Ricci <br9191fr@gmail.com>
2021-12-31 10:20:28 +0100

    Step OK

---
+++ ReadMe/Book.swift
@@ -0,0 +1,16 @@
+//
+//  Book.swift
+//  ReadMe
+//
+//  Created by Bruno Ricci on 31/12/2021.
+//
+
+struct Book {
+    let title: String
+    let author: String
+    
+    init(title: String = "Title", author: String = "Author") {
+        self.title = title
+        self.author = author
+    }
+}
--- ReadMe/ContentView.swift
+++ ReadMe/ContentView.swift
@@ -9,8 +9,23 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        BookRow(book: .init())
+    }
+}
+
+struct BookRow: View {
+    let book: Book
+    var body: some View {
+        HStack {
+            Book.Image(title: book.title)
+            VStack(alignment: .leading) {
+                Text(book.title)
+                    .font(.title2)
+                Text(book.author)
+                    .font(.title3)
+                    .foregroundColor(.secondary)
+            }
+        }
     }
 }
 
